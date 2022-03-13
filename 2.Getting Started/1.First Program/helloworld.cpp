09d752ab7cbac8464ace6f1720a8e7495835c6e5
mandalmohan2001 <101509896+mandalmohan2001@users.noreply.github.com>
2022-03-13 18:59:01 +0530

    Create helloworld.cpp

---
+++ 2.Getting Started/1.First Program/helloworld.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // preprocessor directive
+using namespace std; // namespace directory
+
+int main() { // main method where program execution starts
+    cout << "Hello" << endl; // print "Hello"
+    return 0;
+}
