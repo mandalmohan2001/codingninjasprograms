3cd0f18ef767e2cd72007a2990290daf1a898b0a
mandalmohan2001 <101509896+mandalmohan2001@users.noreply.github.com>
2022-04-06 21:12:15 +0530

    Create pointers_operator.cpp

---
+++ 13.Pointers/2.Pointer Arithmetic/pointers_operator.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i = 10;
+	int * p = &i;
+
+	cout << p << endl;
+	p = p + 2;
+	cout << p << endl;	
+
+	cout << p << endl;
+	p = p - 2;
+	cout << p << endl;	
+
+	double d = 102.3;
+	double * dp = &d;
+	cout << dp << endl;
+	dp++;
+	cout << dp << endl;
+
+	return 0;
+}
