9e95520b0345becf6d78e30fc140bebe8184d0f4
mandalmohan2001 <101509896+mandalmohan2001@users.noreply.github.com>
2022-04-18 12:50:37 +0530

    Create CheckNumber.cpp

---
+++ 15.Recursion 1/4.Recursion and Array/CheckNumber.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+bool checkNumber(int input[], int size, int x) {
+	if(size == 0) {
+		return false;
+	}
+	if(input[0] == x) {
+		return true;
+	}
+	int output = checkNumber(input + 1, size - 1, x);
+	return output;
+} 
+
+int main() {
+	int n;
+	cin >> n;
+
+	int *input = new int[n];
+
+	for(int i = 0; i < n; i++) {
+		cin >> input[i];
+	}	
+
+	int x;
+
+	cin >> x;
+
+	if(checkNumber(input, n, x)) {
+		cout << "true" << endl;
+	} else {
+		cout << "false" << endl;
+	}
+	return 0;
+}
