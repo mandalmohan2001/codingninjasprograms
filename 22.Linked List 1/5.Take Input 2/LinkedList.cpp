01ecbb22fb4d0afc0dbd39842ea3b6471534e132
mandalmohan2001 <101509896+mandalmohan2001@users.noreply.github.com>
2022-04-30 22:00:54 +0530

    Create LinkedList.cpp

---
+++ 22.Linked List 1/5.Take Input 2/LinkedList.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+using namespace std;
+#include "Node.cpp"
+
+Node* takeInput_Better() {
+	int data;
+	cin >> data;
+	Node *head = NULL;
+	Node *tail = NULL;
+	while(data != -1) {
+		Node *newNode = new Node(data);
+		if(head == NULL) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			tail -> next = newNode;
+			tail = tail -> next;
+			// OR
+			// tail = newNode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+void print(Node *head) {
+	// Node *temp = head;
+	
+	while(head != NULL) {
+		cout << head -> data << " ";
+		head = head -> next;
+	}
+
+	/*
+	temp = head;
+	while(temp != NULL) {
+		cout << temp -> data << " ";
+		temp = temp -> next;
+	}
+	*/
+}
+
+int main() {
+	
+	Node *head = takeInput_Better();
+	print(head);
+
+
+	/*
+	// Statically 
+	Node n1(1);
+	Node *head = &n1;
+
+	Node n2(2);
+
+	Node n3(3);
+	Node n4(4);
+	Node n5(5);
+
+	n1.next = &n2;
+	n2.next = &n3;
+	n3.next = &n4;
+	n4.next = &n5;
+
+	print(head);
+
+	print(head);
+	*/
+
+	/*
+	n1.next = &n2;
+
+	cout << n1.data << " " << n2.data << endl;	
+	
+	// Dynamically  
+	Node *n3 = new Node(10);
+	Node *head = n3;
+
+	Node *n4 = new Node(20);
+	n3 -> next = n4;
+	*/
+
+	return 0;
+}
