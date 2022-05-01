055b475ed191b584ec4a9ab7b55b9b3097b226b4
mandalmohan2001 <101509896+mandalmohan2001@users.noreply.github.com>
2022-05-01 22:12:50 +0530

    Create LinkedList.cpp

---
+++ 23.Linked List 2/4.Reverse LL (Recursive)/LinkedList.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+#include "Node.cpp"
+
+Node* reverseLL(Node *head) {
+	if(head == NULL || head -> next == NULL) {
+		return head;
+	}
+
+	Node *smallAns = reverseLL(head -> next);
+
+	Node *temp = smallAns;
+	while(temp -> next != NULL) {
+		temp = temp -> next;
+	}
+	temp -> next = head;
+	head -> next = NULL;
+	return smallAns;
+}
+
+Node* takeInput_Better() {
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while(data != -1) {
+		Node *newNode = new Node(data);
+		if(head == NULL) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			tail -> next = newNode;
+			tail = newNode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+void print(Node *head) {
+	Node *temp = head;
+	while(temp != NULL) {
+		cout << temp -> data << " ";
+		temp = temp -> next;
+	}
+	cout << endl;
+}
+
+int main() {
+	Node *head = takeInput_Better();
+	
+	head = reverseLL(head);
+	print(head);
+	return 0;
+}
