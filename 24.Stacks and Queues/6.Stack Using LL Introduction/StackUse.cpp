af9ad9daab6e518b35561c89d9f46da24d60f1b5
mandalmohan2001 <101509896+mandalmohan2001@users.noreply.github.com>
2022-05-03 16:23:27 +0530

    Create StackUse.cpp

---
+++ 24.Stacks and Queues/6.Stack Using LL Introduction/StackUse.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+#include "Stack.cpp"
+
+int main() {
+	Stack<int> s;
+
+	s.push(10);
+	s.push(20);
+	s.push(30);
+	s.push(40);
+	s.push(50);
+	s.push(100);
+
+	cout << s.top() << endl;
+	cout << s.pop() << endl;
+	cout << s.pop() << endl;
+	cout << s.pop() << endl;
+
+	cout << s.getSize() << endl;
+
+	cout << s.isEmpty() << endl;
+	return 0;
+}
