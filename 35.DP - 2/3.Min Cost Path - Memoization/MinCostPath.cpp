0298ede9f1d4825ecb0af08aea7cf4735239a953
mandalmohan2001 <101509896+mandalmohan2001@users.noreply.github.com>
2022-06-10 14:05:03 +0530

    Create MinCostPath.cpp

---
+++ 35.DP - 2/3.Min Cost Path - Memoization/MinCostPath.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int minCostPath_Mem(int **input, int m, int n, int i, int j, int **output) {
+	if(i == m - 1 && j == n - 1) {
+		return input[i][j];
+	}
+
+	if(i == m || j == n) {
+		return INT_MAX;
+	}
+
+	// Check if output already exists
+	if(output[i][j] != -1) {
+		return output[i][j];
+	}
+
+	// Recursive call
+	int x = minCostPath_Mem(input, m, n, i, j + 1, output);
+	int y = minCostPath_Mem(input, m, n, i + 1, j + 1, output);
+	int z = minCostPath_Mem(input, m, n, i + 1, j, output);
+
+	// Small Calculation
+	int ans = min(x, min(y, z)) + input[i][j];
+
+	// Save the answer for future use
+	output[i][j] = ans;
+
+	return output[i][j];
+}
+
+int minCostPath(int **input, int m, int n) {
+	int **output = new int*[m];
+	for(int i = 0; i < m; i++) {
+		output[i] = new int[n];
+		for(int j = 0; j < n; j++) {
+			output[i][j] = -1;
+		}
+	}
+	return minCostPath_Mem(input, m, n, 0, 0, output);
+}
+
+int main() {
+	int m, n;
+	cin >> m >> n;
+	int **input = new int*[m];
+	for(int i = 0; i < m; i++) {
+		input[i] = new int[n];
+		for(int j = 0; j < n; j++) {
+			cin >> input[i][j];
+		}
+	}
+	cout << minCostPath(input, m, n) << endl;
+	for(int i = 0; i < m; i++) {
+		delete [] input[i];
+	}
+	delete [] input;
+	return 0;
+}
